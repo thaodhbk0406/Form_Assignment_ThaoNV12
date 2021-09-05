960f0efde61d168d42fbbebfec93eefe48ec8f15
thao0406 <thaodhbk95@gmail.com>
2021-09-05 10:31:00 +0700

    Form Assignment ThaoNV12

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,24 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Form-Assignment';
+  subcriptions = ['Basic', 'Advanced', 'Pro'];
+  selectedSubcription = 'Advanced';
+  isSubmitted = false;
+
+  data= {
+    email: '',
+    subcription: '',
+    password: '',
+  }
+
+  @ViewChild('form') sgnForm: NgForm;
+  onSubmit() {
+    console.log(this.sgnForm.value);
+
+    this.data.email = this.sgnForm.value.email;
+    this.data.subcription = this.sgnForm.value.subcription;
+    this.data.password = this.sgnForm.value.password;
+    this.isSubmitted = true;
+    this.sgnForm.form.reset();
+  }
 }
